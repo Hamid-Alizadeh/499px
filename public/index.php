da48d7972c99ae909c813374b0ef6ae5ed2d63dd
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-17 20:58:23 +0330

    Modify index file page title

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>499px</title>
+    <title>499px | Share Your Photos Then Get Rated</title>
 </head>
 
 <body>
