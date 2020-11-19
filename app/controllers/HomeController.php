80215b5cb04facdfbf708a4d46eb86cc5dc3e690
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-19 23:09:19 +0330

    Edit HomeController to extends Controller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,12 +1,12 @@
 <?php
 
-class HomeController
+class HomeController extends Controller
 {
     public function index()
     {
         echo "Hello From Home Page";
     }
-    
+
     public function help()
     {
         echo "This Is Help Page";
