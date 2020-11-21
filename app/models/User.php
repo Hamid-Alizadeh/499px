0ff3d2476389e54a25a99e8b6bb0e44fa3362c2e
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-21 23:43:02 +0330

    Create User.php model to load user info from db

---
+++ app/models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+class User
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new DB;
+    }
+
+    public function getByEmail($email)
+    {
+        $sql = "SELECT * FROM users WHERE email = :email";
+
+        $this->db->query($sql);
+
+        $this->db->bind(":email", $email);
+
+        return $this->db->single();
+    }
+}
