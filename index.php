e276d1be95a74a976e5b28145456514b8decab31
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-21 23:40:25 +0330

    Create index in root directory

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+define('SITENAME', '499px');
+
+require_once './app/require.php';
+
+$controllerReq = isset($_GET['c']) ? $_GET['c'] : 'Home';
+$actionReq = isset($_GET['a']) ? $_GET['a'] : 'index';
+
+$controllerName = ucfirst($controllerReq) . 'Controller';
+$controllerFile = './app/controllers/' . $controllerName . '.php';
+
+if (file_exists($controllerFile)) {
+
+    include $controllerFile;
+
+    $controller = new $controllerName();
+
+    if (method_exists($controller, $actionReq)) {
+        $controller->$actionReq();
+    } else {
+        $controller->index();
+    }
+} else {
+
+    include './app/controllers/HomeController.php';
+
+    $controller = new HomeController();
+    $controller->index();
+}
