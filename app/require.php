dff866eb9b4e9a03a84a133bcc8229b55099f896
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-21 23:41:01 +0330

    Update require.php to load all needed files and classes

--- app/require.php
+++ app/require.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once '../app/models/User.php';
+require_once './app/libs/DB.php';
+require_once './app/libs/Controller.php';
+require_once './app/libs/View.php';
+
+require_once './app/models/User.php';
