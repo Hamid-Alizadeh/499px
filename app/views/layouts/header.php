36a2e2ea18a3a0fb0aa3905098a328c86e3e22e8
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-28 21:34:44 +0330

    Fix login and register link

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -43,8 +43,8 @@
             </form>
 
             <ul class="nav navbar-nav navbar-right">
-                <li><a class="nav-link" href="index.php?c=user&a=register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a class="nav-link" href="index.php?c=user&a=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a class="nav-link" href="index.php?c=home&a=register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a class="nav-link" href="index.php?c=home&a=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
             </ul>
         </div>
     </nav>
