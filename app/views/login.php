18b5dc2db7889e842c4b45e307d7e8328dc67085
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-28 21:35:30 +0330

    Add simple login view file

---
+++ app/views/login.php
@@ -0,0 +1,24 @@
+<?php include './app/views/layouts/header.php'; ?>
+
+
+<div class="container">
+    <div class="row">
+        <form class="form-signin col-md-4 offset-md-4 text-center mt-5">
+            <img class="mb-4" src="/public/images/logo-icon.png" alt="" width="72">
+            <!-- <label for="inputEmail" class="sr-only">Email address</label> -->
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+            <!-- <label for="inputPassword" class="sr-only mt-1">Password</label> -->
+            <input type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
+            <div class="checkbox mb-2 text-left">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="btn btn-lg btn-dark btn-block" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">© 2020</p>
+        </form>
+    </div>
+</div>
+
+
+<?php include './app/views/layouts/footer.php'; ?>
