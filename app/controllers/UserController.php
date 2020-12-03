9edf44f774a7790171e7a3ab3ed91fd5e38b2452
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-12-03 23:39:13 +0330

    Add session value if user login true

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,9 +4,27 @@ class UserController extends Controller
 {
     public function login()
     {
-        echo "<pre>";
-        print_r($_POST);
-        echo "</pre>";
+        $userModel = $this->setModel('User');
+        $user = $userModel->getByEmail($_POST['email']);
+
+        if (!$user) {
+            $this->go('home', 'login');
+        }
+
+        if (password_verify($_POST['password'], $user->password)) {
+            $_SESSION["userId"] = $user->id;
+            $_SESSION["userIsAdmin"] = $user->is_admin;
+            $_SESSION["userName"] = $user->name;
+            $_SESSION["userEmail"] = $user->email;
+        }
+
+        if (isset($_SESSION["userId"]) && $_SESSION['userIsAdmin']) {
+            $this->go('admin', 'dashboard');
+        } else if (isset($_SESSION["userId"])) {
+            $this->go('user', 'profile');
+        } else {
+            $this->go('home', 'login');
+        }
     }
 
     public function register()
