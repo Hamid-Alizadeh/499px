dccadb3dc888897358580a78e0563207427412b4
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-12-08 23:34:34 +0330

    Edit login file to remove comments

--- app/views/login.php
+++ app/views/login.php
@@ -5,9 +5,7 @@
     <div class="row">
         <form action="index.php?c=user&a=login" method="post" class="form-signin col-md-4 offset-md-4 text-center mt-5">
             <img class="mb-4" src="/public/images/logo-icon.png" alt="" width="72">
-            <!-- <label for="inputEmail" class="sr-only">Email address</label> -->
             <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-            <!-- <label for="inputPassword" class="sr-only mt-1">Password</label> -->
             <input name="password" type="password" id="inputPassword" class="form-control mt-1" placeholder="Password" required="">
             <div class="checkbox mb-2 text-left">
                 <label>
