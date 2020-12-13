312bf9278c1b3bdfdf08013067482aea75772a49
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-12-13 23:44:57 +0330

    Update home.php view

--- app/views/home.php
+++ app/views/home.php
@@ -1,5 +1,7 @@
 <?php include './app/views/layouts/header.php'; ?>
 
-<h1>Hello from Home Page</h1>
+<div class="container">
+  <h1>Hello from Home Page</h1>
+</div>
 
 <?php include './app/views/layouts/footer.php'; ?>
