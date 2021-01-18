b6b42d0f5a56ec104b9a55c47ac701e43554ed61
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2021-01-18 23:29:54 +0330

    Update help.php

--- app/views/help.php
+++ app/views/help.php
@@ -1,5 +1,5 @@
 <?php include './app/views/layouts/header.php'; ?>
 
 <h1>How can we help you?</h1>
-<p>email us</p>
+<p>email us : info@499px.tk</p>
 <?php include './app/views/layouts/footer.php'; ?>
