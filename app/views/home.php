5d9ac714e111a66756a548f71aacfd018d1b402b
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2021-02-13 21:48:43 +0330

    Update home.php

--- app/views/home.php
+++ app/views/home.php
@@ -2,7 +2,7 @@
 
 <div class="container">
   <h1>Hello from Home Page</h1>
-  <p>It is a directory for photos </p>
+  <p>It is a directory for photos a clone of 500px </p>
 </div>
 
 <?php include './app/views/layouts/footer.php'; ?>
